8825c9864786a761dbd0c72d12b763e2e1ccde44
nitya <nitya.babbar@gmail.com>
2025-04-27 22:30:21 +0000

    def/use sets for each basic block

--- src/regalloc.rs
+++ src/regalloc.rs
@@ -1,4 +1,5 @@
 use crate::{cfg::CFG, tac::Instruction, x86::X86Operand};
+use crate::tac::Operand;
 use std::{cell::RefCell, collections::{BTreeMap, HashMap, HashSet}};
 
 // Def and reachable uses must be in same web
@@ -25,16 +26,113 @@ pub struct Edge {
     pub dest_web: i32,
 }
 
-
+struct DefUse {
+    defs: HashSet<String>, // all variables that are defined in this basic block
+    uses: HashSet<String> // all variables that are used in this basic block
+}
 
 // Initialize a counter for naming webs
 thread_local! {
     static WEB_COUNTER: RefCell<usize> = RefCell::new(0);
 }
 
+// only consider local variables for now (no array elements)
+fn compute_def_use_sets(method_cfg: &CFG) -> BTreeMap<i32, DefUse> {
+    let mut block_def_use = BTreeMap::new();
 
-///
-fn compute_live_ranges(method_cfg: &CFG) -> BTreeMap<i32, Web>{ 
+    for (block_id, _) in method_cfg.blocks.iter() {
+        let mut def_set = HashSet::new();
+        let mut use_set = HashSet::new();
+
+        for instr in method_cfg
+            .blocks
+            .get(block_id)
+            .unwrap()
+            .instructions
+            .iter()
+        {
+            match instr {
+                Instruction::Assign { src, dest, .. } => {
+                    process_use(&mut use_set, &def_set, src);
+                    process_def(&mut def_set, dest);
+                }
+                Instruction::Add { left, right, dest, .. }
+                | Instruction::Subtract { left, right, dest, .. }
+                | Instruction::Multiply { left, right, dest, .. }
+                | Instruction::Divide { left, right, dest, .. }
+                | Instruction::Modulo { left, right, dest, .. }
+                | Instruction::Greater { left, right, dest }
+                | Instruction::Less { left, right, dest }
+                | Instruction::LessEqual { left, right, dest }
+                | Instruction::GreaterEqual { left, right, dest }
+                | Instruction::Equal { left, right, dest }
+                | Instruction::NotEqual { left, right, dest } => {
+                    process_use(&mut use_set, &def_set, left);
+                    process_use(&mut use_set, &def_set, right);
+                    process_def(&mut def_set, dest);
+                }
+                Instruction::Not { expr, dest }
+                | Instruction::Cast { expr, dest, .. }
+                | Instruction::Len { expr, dest, .. }
+                | Instruction::LoadString { src: expr, dest } => {
+                    process_use(&mut use_set, &def_set, expr);
+                    process_def(&mut def_set, dest);
+                }
+                Instruction::MethodCall { args, dest, .. } => {
+                    for arg in args {
+                        process_use(&mut use_set, &def_set, arg);
+                    }
+                    if let Some(d) = dest {
+                        process_def(&mut def_set, d);
+                    }
+                }
+                Instruction::CJmp { condition, .. } => {
+                    process_use(&mut use_set, &def_set, condition);
+                }
+                Instruction::Ret { value, .. } => {
+                    if let Some(v) = value {
+                        process_use(&mut use_set, &def_set, v);
+                    }
+                }
+                Instruction::LoadConst { dest, .. } => {
+                    process_def(&mut def_set, dest);
+                }
+                Instruction::Exit { .. } 
+                | Instruction::UJmp { .. } => {
+                    // no defs or uses
+                }
+            }
+        }
+
+        block_def_use.insert(block_id.clone(), DefUse { defs: def_set, uses: use_set });
+    }
+
+    block_def_use
+}
+
+fn process_use(use_set: &mut HashSet<String>, def_set: &HashSet<String>, operand: &Operand) {
+    if let Some(name) = get_local_var_name(operand) {
+        // don't include uses of variables that were defined within this basic block
+        if !def_set.contains(&name) {
+            use_set.insert(name);
+        }
+    }
+}
+
+fn process_def(def_set: &mut HashSet<String>, operand: &Operand) {
+    if let Some(name) = get_local_var_name(operand) {
+        def_set.insert(name);
+    }
+}
+
+fn get_local_var_name(operand: &Operand) -> Option<String> {
+    match operand {
+        Operand::LocalVar(name, _) => Some(name.clone()),
+        _ => None
+    }
+}
+
+fn compute_live_ranges(method_cfg: &CFG) -> BTreeMap<i32, Web> { 
     todo!()
 }
 
